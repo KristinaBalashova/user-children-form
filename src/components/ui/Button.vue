d158d5f8c8f72e353e2b2889d4c62530352be220
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-12 17:46:11 +0200

    add: input & button ui with vuetify

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    default: 'primary',
+  },
+  variant: {
+    type: String,
+    default: 'outlined',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <v-btn
+    v-bind="$attrs"
+    :variant="variant"
+    class="btn"
+    :class="[`btn--${color}`, variant]"
+    :disabled="disabled"
+  >
+    <slot />
+  </v-btn>
+</template>
---
+++ src/components/ui/Input.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  variant: {
+    type: String,
+    default: 'solo-filled',
+  },
+});
+
+const modelValue = defineModel();
+</script>
+
+<template>
+  <v-text-field
+    v-bind="$attrs"
+    :model-value="modelValue"
+    @update:model-value="modelValue = $event"
+    :placeholder="placeholder"
+    class="input"
+    :variant="variant"
+    clearable
+  />
+</template>
