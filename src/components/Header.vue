56cb8dd7d989488b00bb9af1e9180d49c357e5d3
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-12 17:47:11 +0200

    add: header

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<script setup>
+import logo from '../assets/logo.svg'
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo">
+      <img :src="logo" alt="Logo" />
+    </div>
+    <nav class="nav">
+      <ul class="nav-list">
+        <li><a href="/">Форма</a></li>
+        <li><a href="/preview">Превью</a></li>
+      </ul>   
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 76px;
+  padding: 0 24px;
+  border-bottom: 1px solid #eee;
+}
+
+.logo img {
+  height: 40px;
+}
+
+.nav {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+}
+
+.nav-list {
+  list-style: none;
+  display: flex;
+  gap: 32px;
+  padding: 0;
+  margin: 0;
+}
+
+.nav-list li a {
+  text-decoration: none;
+  color: #444;
+  font-weight: 500;
+}
+
+</style>
