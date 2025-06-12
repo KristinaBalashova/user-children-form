c1116312ee96c049bb14b0c7f6afe9f76bce5c50
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-12 17:20:10 +0200

    initial: base files

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { createPinia } from 'pinia'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const pinia = createPinia()
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(pinia).use(vuetify).mount('#app')
