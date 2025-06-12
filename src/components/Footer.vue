2d2d61db0c88c5aa5d1f889e8fb4095f2c420086
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-12 17:46:45 +0200

    add: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer">
+    <p class="text">all rights reserved</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #FAFAFA;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 76px;
+}
+
+.text {
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 100%;
+  letter-spacing: 0px;
+  vertical-align: bottom;
+  margin: 0;
+}
+</style>
