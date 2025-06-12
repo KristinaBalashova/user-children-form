ddd2675ce0af05b4bc5e27cb14396ff32a037e7f
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-12 17:45:28 +0200

    add: basic layout styles

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,40 @@ import Header from './components/Header.vue';
 </script>
 
 <template>
+   <div class="layout">
     <Header />
-    <router-view />
+    <main class="main">
+      <router-view />
+    </main>
     <Footer />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.layout > *:first-child {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background: white; /* или нужный фон */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+
+.main {
+  flex: 1;
+  padding: 1rem;
+  overflow-y: auto,
+}
+
+.footer {
+  background-color: #f8f8f8;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
