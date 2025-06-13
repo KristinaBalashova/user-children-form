232a71546d7777704d18359c6e30e666e2b85bff
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-13 12:44:41 +0200

    fix: make custom input

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,30 +1,82 @@
 <script setup>
+import { ref } from "vue";
+
+const value = ref("");
+const isFocused = ref(false);
+
 defineProps({
-  placeholder: {
+  label: {
     type: String,
-    default: '',
+    default: "",
   },
   variant: {
     type: String,
-    default: 'outlined',
+    default: "outlined",
   },
 });
+
+function onBlur() {
+  if (!value.value) {
+    isFocused.value = false;
+  }
+}
 
 const modelValue = defineModel();
 </script>
 
 <template>
-  <v-text-field
-    v-bind="$attrs"
-    :model-value="modelValue"
-    @update:model-value="modelValue = $event"
-    :placeholder="placeholder"
-    class="input"
-    :variant="variant"
-    clearable
-  />
+  <div class="input-wrapper">
+    <input
+      v-bind="$attrs"
+      v-model="modelValue"
+      required
+      class="input"
+      @focus="isFocused = true"
+      @blur="onBlur"
+      :variant="variant"
+    />
+    <label :class="{ floated: isFocused || modelValue }">{{ label }}</label>
+  </div>
 </template>
 
 <style scoped>
+.input-wrapper {
+  position: relative;
+}
 
+.input {
+  width: 100%;
+  padding: 14px 12px 6px 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  outline: none;
+  background: transparent;
+}
+
+label {
+  position: absolute;
+  top: 14px;
+  left: 12px;
+  color: #999;
+  font-size: 14px;
+  pointer-events: none;
+  transition: 0.2s ease all;
+  background-color: white;
+  padding: 0 4px;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0px;
+}
+
+label.floated {
+  top: 4px;
+  left: 10px;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 100%;
+  letter-spacing: 0px;
+  color: #1111117A;
+
+}
 </style>
