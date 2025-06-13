b3353a25f3c29ac4a060a00f47768d2a62561697
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-13 18:25:00 +0200

    add: preview page logic

--- src/views/PreviewPage.vue
+++ src/views/PreviewPage.vue
@@ -1,24 +1,32 @@
 <script setup>
 import { useUser } from "../store/user";
+import { computed } from "vue";
 
 const userStore = useUser();
+
+const { name, age } = userStore;
+
+const children = computed(() => userStore.children);
 </script>
 
 <template>
   <div class="preview">
-    <section>
+    <section class="parent-info">
       <h2>Персональные данные</h2>
-      <p class="person">{{ userStore.name }}, {{ userStore.age }} лет</p>
+
+      <p class="user-info-text" v-if="name && age">{{ name }}, {{ age }} лет</p>
+      <p v-else>Нет персональных данных</p>
     </section>
-    <section>
+    <section class="children-info">
       <h2>Дети</h2>
 
-      <ul v-if="userStore.children.length" class="children">
+      <ul v-if="children.length > 0" class="children">
         <li
-          v-for="(child, index) in userStore.children"
-          :key="child.id || index"
+          v-for="(child, index) in children"
+          :key="index"
+          class="user-info-text"
         >
-          {{ child.name }}, {{ child.age }} {{ getAgeLabel(child.age) }}
+          {{ child.name }}, {{ child.age }} лет
         </li>
       </ul>
 
@@ -28,6 +36,13 @@ const userStore = useUser();
 </template>
 
 <style scoped>
+.parent-info {
+  margin-bottom: 60px;
+}
+
+.children-info {
+}
+
 .preview {
   max-width: 600px;
   margin: 0 auto;
@@ -35,16 +50,21 @@ const userStore = useUser();
 }
 
 h2 {
-  font-size: 14px;
-  font-weight: 400;
-  color: #444;
-  margin-bottom: 8px;
+  color: #111111;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0px;
+  margin: 0;
+  margin-bottom: 20px;
 }
 
-.person {
+.user-info-text {
   font-weight: 700;
   font-size: 16px;
-  margin-bottom: 24px;
+  line-height: 24px;
+  letter-spacing: 0px;
+  color: #111111;
 }
 
 .children {
@@ -53,11 +73,11 @@ h2 {
   display: flex;
   flex-direction: column;
   gap: 12px;
+  gap: 20px;
 }
 
 .children li {
-  background: #f0f0f0;
-  font-weight: 700;
+  background-color: #F1F1F1;
   padding: 8px 16px;
   border-radius: 6px;
   display: inline-block;
