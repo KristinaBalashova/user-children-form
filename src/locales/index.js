696f06c860a69c008c5cadefd6ba2806a27425c3
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-06-13 15:07:46 +0200

    add: i18n

---
+++ src/locales/index.js
@@ -0,0 +1,25 @@
+import PreviewPage from "../views/PreviewPage.vue";
+
+export const messages = {
+  ru: {
+    common: {
+      form: "Форма",
+      preview: "Предпросмотр",
+      delete: "Удалить",
+      allRightsReserved: "Все права защищены",
+      save: 'Сохранить',
+    },
+    form: {
+      name: "Имя",
+      age: "Возраст",
+      submit: "Отправить",
+      addChild: "Добавить ребенка",
+      personalData: "Персональные данные",
+      kids: "Дети",
+    },
+    errors: {
+      required: "Обязательное поле",
+      invalidAge: "Возраст должен быть неотрицательным",
+    },
+  },
+};
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './style.css'
+import { createI18n } from 'vue-i18n';
+import { messages } from './locales';
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ru',
+  fallbackLocale: 'en',
+  messages,
+});
+
 
 const pinia = createPinia()
 
